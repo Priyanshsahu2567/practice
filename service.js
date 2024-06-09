dbd4360c4b8ba96a7842721548687f00ce7d69b3
Priyanshsahu2567 <priyanshsahu7828@gmail.com>
2024-06-09 12:56:35 +0530

    Update service.js

--- service.js
+++ service.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express();
 app.use(express.json());
-
+const dotenv = require('dotenv');
 const mongoose =require('mongoose');
-mongoose.connect('mongodb://localhost:27017/e-com')
 
+
+dotenv.config({
+    path:'./config/config.env'
+});
+
+const url = process.env.MONGO_URI
+const PORT = process.env.PORT;
+mongoose.connect(url)
+console.log('connected')
 
 app.use('/auth',require('./user_routes'));
 
-app.listen(3000);
+app.listen(PORT,console.log('running'));
